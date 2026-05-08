df878141820df7cc0940cc65e85949a43297d261
xuxin1955 <x2874836915@qq.com>
2026-05-08 16:05:14 +0800

    update

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,21 +1,8 @@
 #!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
 
 # 8916dts
 mkdir -p target/linux/msm89xx/dts/
 cp -f "$GITHUB_WORKSPACE/scripts/dts/msm8916.dtsi" "target/linux/msm89xx/dts/msm8916.dtsi"
-
-# aic8800
-sed -i -e 's/aic8800DC_fdrv\.ko/aic8800_fdrv.ko/g' -e 's/aic_load_fw aic8800DC_fdrv/aic_load_fw aic8800_fdrv/g' package/kernel/aic8800/Makefile
-
 
 # OpenClash
 # git clone --depth 1 https://github.com/vernesong/OpenClash.git OpenClash
@@ -38,6 +25,3 @@ sed -i 's|src-git-full openstick https://github.com/lkiuyu/openstick-feeds.git|s
 
 # inspect
 echo ">>> OPP VOLTAGE COUNT: $(grep -c 'opp-microvolt' target/linux/msm89xx/dts/msm8916.dtsi) <<<"
-
-# aic8800
-grep -n "aic8800_fdrv\|aic8800DC_fdrv" package/kernel/aic8800/Makefile
